594169bf85c9438a9ce175d9cc83974f11a64513
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-10-07 10:33:35 +0500

    Version 0.15.2
    PEP8 formatting

--- copeer.py
+++ copeer.py
@@ -3,33 +3,39 @@ from os.path import join
 from shutil import copy2
 from warnings import showwarning
 
-class CopyError(BaseException): pass
 
-def _warn(text:str) -> None:
-	def CopyWarning(): pass
-	showwarning(text, CopyWarning, 'copeer.py', 10)
+class CopyError(BaseException):
+    pass
+
+
+def _warn(text: str) -> None:
+    def CopyWarning(): pass
+    showwarning(text, CopyWarning, 'copeer.py', 10)
+
 
 class CopyFiles:
 
-	def __init__(self, copy_from:str=''):
-		self._path_from = copy_from
+    def __init__(self, copy_from: str=''):
+        self._path_from = copy_from
 
-	def copy(self, paths:list[str]=None) -> None:
-		if paths is None:
-			raise CopyError("Paths not found!")
-		for path_to in paths:
-			if len(listdir(path_to)) != 0:
-				_warn(f"Directory {path_to} not empty. Possible warnings during validation")
-			for filename in listdir(self._path_from):
-				copy2(join(self._path_from, filename), path_to)
+    def copy(self, paths: list[str]=None) -> None:
+        if paths is None:
+            raise CopyError("Paths not found!")
+        for path_to in paths:
+            if len(listdir(path_to)) != 0:
+                _warn(
+                    f"Directory {path_to} not empty. Possible warnings during validation")
+            for filename in listdir(self._path_from):
+                copy2(join(self._path_from, filename), path_to)
 
-	def validate(self, paths:list[str]=None) -> bool:
-		original_files = listdir(self._path_from)
-		checker = []
-		for path in paths:
-			if listdir(path) != original_files:
-				_warn(f'There are extraneous files in the directory. Perhaps this is a mistake')
-				checker.append(False)
-			else:
-				checker.append(True)
-		return all(checker)
+    def validate(self, paths: list[str]=None) -> bool:
+        original_files = listdir(self._path_from)
+        checker = []
+        for path in paths:
+            if listdir(path) != original_files:
+                _warn(
+                    f'There are extraneous files in the directory. Perhaps this is a mistake')
+                checker.append(False)
+            else:
+                checker.append(True)
+        return all(checker)
--- linuxdetect.py
+++ linuxdetect.py
@@ -2,46 +2,48 @@ from os import popen, listdir
 from .copeer import CopyFiles
 from .abcdetector import ABCDetect, ABCDetectError
 
+
 class LinuxDetectError(ABCDetectError):
-	pass
+    pass
+
 
 class Detector(ABCDetect):
 
-	def __init__(self, copeer:CopyFiles=None):
-		if not copeer:
-			raise LinuxDetectError('Copeer not found!')
-		self.copeer = copeer
-		self._paths = []
+    def __init__(self, copeer: CopyFiles=None):
+        if not copeer:
+            raise LinuxDetectError('Copeer not found!')
+        self.copeer = copeer
+        self._paths = []
 
-	def _start_copy(self) -> None:
-		self.copeer.copy(self._paths)
-	
-	def _check_empty(self, lst:list[str]) -> bool:
-		return listdir(lst[0]) <= 0
-	
-	def _validate(self, paths:list[str]=None) -> bool:
-		if paths is None:
-			paths = self._paths
-		return self.copeer.validate(paths)
-	
-	def get_usb_devices(self) -> list[str]:
-		mounts_point_list = popen('findmnt').read().splitlines()
-		mounts_point_list_splited = map(lambda x: x.split(), mounts_point_list)
-		for info in filter(lambda x: x[1].startswith('/dev/sd') or x[1].startswith('/dev/mmcblk') or x[1].startswith('/dev/sr'), mounts_point_list_splited):
-			if info[1].startswith('/dev/sd'):
-				for info_line in popen(f'udevadm info --query=all --name={info[1]}').read().splitlines():
-					if (info_line.startswith('E: DEVPATH')) and ('usb' in info_line):
-						if info[1] not in self._paths:
-							self._paths.append(info[0])
-			else:
-				if info[1] not in self._paths:
-					self._paths.append(info[0])
-		return self._paths
+    def _start_copy(self) -> None:
+        self.copeer.copy(self._paths)
 
-	def start_copy(self, todevices:list[str]=None):
-		if (todevices == self._paths) or (todevices is None):
-			self._start_copy()
-			return self._validate()
-		else:
-			self.copeer.copy(todevices)
-			return self._validate(todevices)
+    def _check_empty(self, lst: list[str]) -> bool:
+        return listdir(lst[0]) <= 0
+
+    def _validate(self, paths: list[str]=None) -> bool:
+        if paths is None:
+            paths = self._paths
+        return self.copeer.validate(paths)
+
+    def get_usb_devices(self) -> list[str]:
+        mounts_point_list = popen('findmnt').read().splitlines()
+        mounts_point_list_splited = map(lambda x: x.split(), mounts_point_list)
+        for info in filter(lambda x: x[1].startswith('/dev/sd') or x[1].startswith('/dev/mmcblk') or x[1].startswith('/dev/sr'), mounts_point_list_splited):
+            if info[1].startswith('/dev/sd'):
+                for info_line in popen(f'udevadm info --query=all --name={info[1]}').read().splitlines():
+                    if (info_line.startswith('E: DEVPATH')) and ('usb' in info_line):
+                        if info[1] not in self._paths:
+                            self._paths.append(info[0])
+            else:
+                if info[1] not in self._paths:
+                    self._paths.append(info[0])
+        return self._paths
+
+    def start_copy(self, todevices: list[str]=None):
+        if (todevices == self._paths) or (todevices is None):
+            self._start_copy()
+            return self._validate()
+        else:
+            self.copeer.copy(todevices)
+            return self._validate(todevices)
