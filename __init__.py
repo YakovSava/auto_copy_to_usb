7c464ccc2c8a9a896f9e268660f9b2f764138035
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-10-12 19:35:52 +0500

    Version 0.16

--- __init__.py
+++ __init__.py
@@ -10,9 +10,15 @@ class NotSupported(BaseException):
 if platform == "win32":
     from .windetect import Detector
 elif platform.startswith('linux'):
+<<<<<<< HEAD
+	from .linuxdetect import Detector
+elif platform == "darwin":
+	from .macdetect import Detector
+=======
     from .linuxdetect import Detector
 elif platform == "darwin":
     from .macdetect import Detector
+>>>>>>> 594169bf85c9438a9ce175d9cc83974f11a64513
 else:
     raise NotSupported(f"Platform \"{platform}\" not supported!")
 
--- macdetect.py
+++ macdetect.py
@@ -2,11 +2,18 @@ from os import popen, listdir
 from os.path import isdir
 from .copeer import CopyFiles
 from .abcdetector import ABCDetect, ABCDetectError
+<<<<<<< HEAD
+=======
 
+>>>>>>> 594169bf85c9438a9ce175d9cc83974f11a64513
 
 class MacDetectError(ABCDetectError):
     pass
 
+<<<<<<< HEAD
+class Detector(ABCDetect):
+=======
+>>>>>>> 594169bf85c9438a9ce175d9cc83974f11a64513
 
 class Detector(ABCDetect):
 
@@ -20,10 +27,23 @@ class Detector(ABCDetect):
     def _start_copy(self) -> None:
         self.copeer.copy(self._paths)
 
+<<<<<<< HEAD
+	def get_usb_devices(self) -> list[str]:
+		for line in popen('diskutil list | grep /dev/').read().splitlines():
+			#print('Line: ', line)
+			if popen(f'diskutil info {line.split()[0]} | grep Protocol').read().split()[1] == 'USB':
+				mnt_point = ('/Volumes/' + popen(f'diskutil list {line.split()[0]}').read().splitlines()[-1].split()[2])
+				#print(mnt_point)
+				if (isdir(mnt_point) and (mnt_point not in self._paths)):
+					#print("Mount point: ", mnt_point)
+					self._paths.append(mnt_point)
+		return self._paths
+=======
     def _validate(self, paths: list[str]=None) -> None:
         if paths is None:
             paths = self._paths
         return self.copeer.validate(paths)
+>>>>>>> 594169bf85c9438a9ce175d9cc83974f11a64513
 
     def get_usb_devices(self) -> list[str]:
         for line in popen('diskutil list | grep /dev/').read().splitlines():
